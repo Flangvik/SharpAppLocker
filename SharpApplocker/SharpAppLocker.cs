07646b7bbbdb407469cac4dfc07ebdce48181be4
am0nsec <20334678+am0nsec@users.noreply.github.com>
2020-08-01 22:41:42 +0100

    Remove use of inferior functions + fix XML param

--- SharpApplocker/SharpAppLocker.cs
+++ SharpApplocker/SharpAppLocker.cs
@@ -1,44 +1,41 @@
 ﻿using System;
-using Microsoft.Security.ApplicationId.PolicyManagement;
-using Microsoft.Security.ApplicationId.PolicyManagement.PolicyModel;
+using System.Xml;
 using Newtonsoft.Json;
 
-namespace SharpApplocker
-{
-    public class SharpAppLocker
-    {
-        public enum PolicyType
-        {
+namespace SharpApplocker {
+    public class SharpAppLocker {
+        public enum PolicyType {
             Local,
             Domain,
             Effective
         }
 
-        public static string GetAppLockerPolicy(PolicyType policyType, string ldapPath = "", bool xmlOutput = false)
-        {
+        public static string GetAppLockerPolicy(PolicyType policyType, string ldapPath = "", bool xmlOutput = false) {
 
-            AppLockerPolicy appLockerPolicy;
-            if (policyType == PolicyType.Local)
-            {
-                appLockerPolicy = PolicyManager.GetLocalPolicy();
-            }
-            else if (policyType == PolicyType.Domain)
-            {
-                appLockerPolicy = PolicyManager.GetDomainPolicy(ldapPath);
-            }
-            else
-            {
-                if (policyType != PolicyType.Effective)
-                {
+            // Create IAppIdPolicyHandler COM interface
+            IAppIdPolicyHandler IAppHandler = new AppIdPolicyHandlerClass();
+            string policies;
+
+            switch(policyType) {
+                case PolicyType.Local:
+                case PolicyType.Domain:
+                    policies = IAppHandler.GetPolicy(ldapPath);
+                    break;
+
+                case PolicyType.Effective:
+                    policies = IAppHandler.GetEffectivePolicy();
+                    break;
+
+                default:
                     throw new InvalidOperationException();
-                }
-                appLockerPolicy = PolicyManager.GetEffectivePolicy();
             }
+
             if (xmlOutput)
-            {
-                return (appLockerPolicy.ToXml());
-            }
-            return JsonConvert.SerializeObject(appLockerPolicy, Formatting.Indented);
+                return policies;
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(policies);
+            return JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented, true);
         }
     }
 }
